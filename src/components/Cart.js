1bcc76d7be80cb52f302b8661384c64a224b049c
Your Name <henrikh111@outlook.com>
2020-10-01 14:28:03 +0400

    animation-modal

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import formatCurrency from "../util";
-
+import Fade from "react-reveal/Fade"
 export default class Cart extends Component {
   constructor(props) {
     super(props);
@@ -37,6 +37,9 @@ export default class Cart extends Component {
         )}
         <div>
           <div className="cart">
+              <Fade left cascade>
+
+              
             <ul className="cart-items">
               {cartItems.map((item) => (
                 <li key={item._id}>
@@ -58,6 +61,7 @@ export default class Cart extends Component {
                 </li>
               ))}
             </ul>
+            </Fade>
           </div>
           {cartItems.length !== 0 && (
             <div>
@@ -80,7 +84,9 @@ export default class Cart extends Component {
                 </div>
               </div>
               {this.state.showCheckout && (
+                    <Fade right cascade>
                 <div className="cart">
+                  
                   <form onSubmit={this.createOrder}>
                     <ul className="form-container">
                       <li>
@@ -117,7 +123,9 @@ export default class Cart extends Component {
                       </li>
                     </ul>
                   </form>
+                  
                 </div>
+                </Fade>
               )}
             </div>
           )}
