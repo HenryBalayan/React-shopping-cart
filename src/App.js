e2645cb1c49ff3a7d6bd15032814539e738d82b7
Your Name <henrikh111@outlook.com>
2020-10-01 12:33:29 +0400

    cartitems to localestorage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-// feature 1
+
 import React from "react";
 import data from "./data.json";
 import Products from "./components/Products";
 import Filter from "./components/Filter";
 import Cart from "./components/Cart";
-
+// feature 1
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       products: data.products,
-      cartItems:[],
+      cartItems:localStorage.getItem("cartItems")
+      ? JSON.parse(localStorage.getItem("cartItems"))
+      : [],
       size: "",
       sort: "",
     };
   }
+  createOrder = (order) => {
+    alert("Need to save order for " + order.name);
+  };
 
   removeFromCart = (product) => {
     const cartItems = this.state.cartItems.slice();
     this.setState({
       cartItems: cartItems.filter((x) => x._id !== product._id),
     });
+    localStorage.setItem(
+      "cartItems",
+      JSON.stringify(cartItems.filter((x) => x._id !== product._id))
+    );
   };
   addToCart = (product) => {
     const cartItems = this.state.cartItems.slice();
@@ -35,6 +44,7 @@ class App extends React.Component {
       cartItems.push({ ...product, count: 1 });
     }
     this.setState({ cartItems });
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
   };
 
 
@@ -100,6 +110,7 @@ class App extends React.Component {
               <Cart 
               cartItems={this.state.cartItems}
               removeFromCart={this.removeFromCart}
+              createOrder={this.createOrder}
               />
               
               </div>
